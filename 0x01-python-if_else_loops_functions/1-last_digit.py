f9952469d8d42226100365d3ed8c54cfeecc41fb
Kamundia-Simon <simonkamundia8@gmail.com>
2023-09-05 23:51:20 +0300

    checks for sizeof num

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
 import random
+import math
 number = random.randint(-10000, 10000)
 last_d = abs(number) % 10
 if number < 0:
     last_d = -last_d
     print(f"Last digit of {number} is {last_d}", end=" ")
-    if last_d > 5:
-        print("and is greater than 5")
-    elif last_d == 0:
-        print("and is 0")
-    else:
-        print("and is less than 6 and not 0")
+if last_d > 5:
+    print("and is greater than 5")
+elif last_d == 0:
+    print("and is 0")
+else:
+    print("and is less than 6 and not 0")
