65d342756151c3d84ce4081151e92623fca8647d
Kamundia-Simon <simonkamundia8@gmail.com>
2023-10-16 18:24:25 +0300

    models/tests files

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+"""Base Class"""
+
+
+class Base:
+    """a class base"""
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """constuctor"""
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
