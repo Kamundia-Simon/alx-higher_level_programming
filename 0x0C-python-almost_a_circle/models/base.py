5f05223ec7ce653cfae4234e5c3867cd280a0ef9
Kamundia-Simon <simonkamundia8@gmail.com>
2023-10-19 19:15:26 +0300

    Base class

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,1 +1,24 @@
 #!/usr/bin/python3
+
+"""Defines a base model class."""
+
+
+class Base:
+    """rep base model
+    Attributes:
+    __nb_objects: number of instantiated Bases
+    """
+
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """Initialize a new Base.
+
+        Args:
+            id: identity of the new base
+        """
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
