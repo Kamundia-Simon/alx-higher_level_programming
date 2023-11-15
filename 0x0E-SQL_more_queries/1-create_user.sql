8077d91878b64ceb0eeb4ff140986d33fa68ab85
Kamundia-Simon <simonkamundia8@gmail.com>
2023-11-16 00:01:54 +0300

    create user

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,9 +1,7 @@
--- script that creates the database hbtn_0d_2 and the user user_0d_2.
-CREATE USER
-    IF NOT EXISTS 'user_0d_1'@'localhost'
-    IDENTIFIED BY 'user_0d_1_pwd';
-GRANT ALL PRIVILEGES
-   ON *.*
-   TO 'user_0d_1'@'localhost'
-   IDENTIFIED BY 'user_0d_1_pwd';
+-- Creates user
+-- create user
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+-- grant privs
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
+-- flush
 FLUSH PRIVILEGES;
