45e2d7ea8fe6f9c82807a1de10baabe24f94e256
Kamundia-Simon <simonkamundia8@gmail.com>
2024-07-06 00:22:04 +0300

    What is  my status? #1

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+""" python script that fetches https://alx-intranet.hbtn.io/status """
+import requests
+
+
+if __name__ == "__main__":
+    url = 'https://alx-intranet.hbtn.io/status'
+    res = requests.get(url)
+
+    print("Body response:")
+    print("\t- type: {}".format(type(res.text)))
+    print("\t- content: {}".format(res.text))
