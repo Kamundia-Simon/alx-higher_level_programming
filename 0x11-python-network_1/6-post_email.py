565a291e0104ffb015765650293a564a052bdd28
Kamundia-Simon <simonkamundia8@gmail.com>
2024-07-06 00:37:21 +0300

    POST an email #1

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+""" Script that sends a POST request to the passed URL with the email"""
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    email = sys.argv[2]
+
+    data = {'email': email}
+    res = requests.post(url, data=data)
+
+    print(res.text)
