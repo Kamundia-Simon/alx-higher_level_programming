05648b7bf843336c6957ebe985b1ec088700abd1
Kamundia-Simon <simonkamundia8@gmail.com>
2024-07-31 00:36:12 +0300

    Status code

---
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+// a script that display the status code of a GET request.
+
+const request = require('request');
+
+const url = process.argv[2];
+
+request.get(url, (error, response) => {
+  if (error == null) {
+    console.log(`code: ${response.statusCode}`);
+  }
+});
