94b15a60b1c36fbad1ca1c7bc83589092067fef8
Kamundia-Simon <simonkamundia8@gmail.com>
2024-08-08 00:24:21 +0300

    fix

--- 0x15-javascript-web_jquery/8-script.js
+++ 0x15-javascript-web_jquery/8-script.js
@@ -1,5 +1,5 @@
 /* global $ */
-$.get('https://swapi-api.hbtn.io/api/films/?format=json', function (data) {
+$.get('https://swapi-api.alx-tools.com/api/films/?format=json', function (data) {
   console.log(data);
   data.results.forEach(film => {
     $('UL#list_movies').append('<li>' + film.title + '</li>');
